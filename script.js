f177d9a688d5c74c2d0ca8adf7579e808dd887f6
ArjunB126 <arjunbajare86@gmail.com>
2024-01-17 19:11:35 +0530

    Js_Updates

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ let newGameBtn = document.querySelector("#new-btn");
 
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
+
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
