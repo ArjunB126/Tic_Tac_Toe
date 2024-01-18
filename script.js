90322c0918a09560cdd67e8b589d213f6f843bd2
ArjunB126 <arjunbajare86@gmail.com>
2024-01-18 16:49:09 +0530

    Js_Changes

--- script.js
+++ script.js
@@ -84,6 +84,7 @@ const enableBoxes = () =>
 	
 const showWinner = (winner) => {
     msg.innerText = `Congratulations, Winner is ${winner}`;
-
+    msgContainer.classList.remove("hide");
+    disableBoxes();
   };
   
