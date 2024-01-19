0b0ea5618e8f7a2f7eb1d778cb5b126f6b87334f
ArjunB126 <arjunbajare86@gmail.com>
2024-01-19 17:56:47 +0530

    Js_Changes

--- script.js
+++ script.js
@@ -89,16 +89,25 @@ const showWinner = (winner) => {
     disableBoxes();
 };
 
-const checkWinner = () => {
-  for (let pattern of winPatterns) {
+const checkWinner = () => 
+{
+  for (let pattern of winPatterns) 
+  {
     let pos1Val = boxes[pattern[0]].innerText;
     let pos2Val = boxes[pattern[1]].innerText;
     let pos3Val = boxes[pattern[2]].innerText;
 
-    if (pos1Val != "" && pos2Val != "" && pos3Val != ""){
-      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "")
+    {
+      if (pos1Val === pos2Val && pos2Val === pos3Val)
+       {
         showWinner(pos1Val);
         return true;
+      }
+    }
+  }
+};
+
 
 
 newGameBtn.addEventListener("click", resetGame);
